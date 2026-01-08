f13d8f6f6769a0fdbdc6530c08922d7a3ace406e
Felipe <felipervmospinheiro@gmail.com>
2026-01-08 15:13:24 -0300

    Qualidade do Ar

--- Qualidade_do_Ar_em_São_Paulo.py
+++ Qualidade_do_Ar_em_São_Paulo.py
@@ -1,19 +1,19 @@
-# Libraries
+# Bibliotecas
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import datetime
 
-# Pandas to read_csv
+# Pandas para ler CSV
 air_quality = pd.read_csv('sp_air_quality.csv')
 print(air_quality.head(3))
 
-# Inputing NO2 median values by 'Station' sub-group.
+# Preenchendo valores medianos de NO2 por subgrupo de 'Estação'.
 NO2_dict = air_quality.groupby('Station')['NO2'].median().dropna().to_dict()
 air_quality['NO2'] = air_quality['NO2'].fillna(air_quality['Station'].map(NO2_dict))
 print(air_quality.isna().sum())
 
-# Handling with outliers by 'Station' sub-group.
+# Tratando outliers por subgrupo de 'Estação'.
 Q1 = air_quality.groupby('Station')['NO2'].transform(lambda x: x.quantile(0.25))
 Q3 = air_quality.groupby('Station')['NO2'].transform(lambda x: x.quantile(0.75))
 IQR = Q3 - Q1
@@ -24,11 +24,11 @@ upper = Q3 + 1.5 * IQR
 air_quality = air_quality[(air_quality['NO2'] >= lower) & (air_quality['NO2'] <= upper)]
 print(air_quality.head())
 
-# Creating a new column 'Hour'
+# Criando uma nova coluna 'Hour'
 air_quality['Datetime'] = pd.to_datetime(air_quality['Datetime'])
 air_quality['Hour'] = air_quality['Datetime'].dt.hour
 
-# Dict 1 for a new table: region_list
+# Dicionário 1 para uma nova tabela: region_list
 stations = {
     'Station':['Araçatuba', 'Catanduva', 'Pico do Jaraguá', 'Ribeirão Preto','Araraquara', 'São José do Rio Preto', 
 'Bauru', 'Jaú', 'Jundiaí', 'Marília', 'Paulínia', 'Piracicaba', 'Sorocaba', 'Tatuí', 'Campinas-Centro', 'Campinas-V.União', 'Campinas-Taquaral',
@@ -40,7 +40,7 @@ stations = {
 'N.Senhora do Ó', 'Santana', 'Itaim Paulista', 'Itaquera', 'Mooca']
 }
 
-# Dict 2 for a new table: region_list
+# Dicionário 2 para uma nova tabela: region_list
 region =  {
     'Interior': ['Bauru', 'Jaú', 'Jundiaí', 'Marília', 'Paulínia', 'Piracicaba', 'Sorocaba', 'Tatuí', 'Araraquara', 'São José do Rio Preto','Araçatuba', 'Catanduva', 'Ribeirão Preto', 'Rio Claro-Jd.Guanabara', 'Campinas-Centro', 'Campinas-V.União', 'Campinas-Taquaral','Americana','Limeira','Santa Gertrudes'],
     'Região Metropolitana': ['Carapicuíba', 'Diadema', 'Guarulhos-Paço Municipal', 'Guarulhos-Pimentas', 'Mauá', 'Osasco', 'Perus', 'S.André-Capuava', 'S.Bernardo-Centro', 'S.Bernardo-Paulicéia', 'São Caetano do Sul', 'Taboão da Serra', 'Pico do Jaraguá'], 
@@ -49,20 +49,20 @@ region =  {
     'Capital': ['Cerqueira César', 'Ibirapuera', 'Marg.Tietê-Pte Remédios', 'Parque D.Pedro II','N.Senhora do Ó', 'Santana','Itaim Paulista', 'Itaquera', 'Mooca', 'Capão Redondo', 'Congonhas', 'Grajaú-Parelheiros', 'Interlagos', 'Santo Amaro']
 }
 
-# New DataFrame
+# Novo DataFrame
 region_list =pd.DataFrame(stations)
 station_to_region = {station: reg for reg, stations in region.items() for station in stations}
 region_list['Region'] = region_list['Station'].map(station_to_region)
 
-# Final DataFrame
+# DataFrame final
 air_quality_merge = air_quality.merge(region_list, how='left', on='Station')
 
-# Calculation NO2 for each Region using Pandas groupby
+# Cálculo de NO2 para cada Região usando Pandas groupby
 NO2_mean_per_region = air_quality_merge.groupby('Region')['NO2'].mean()
 print(air_quality_merge['Region'].value_counts(normalize=True))
 print(NO2_mean_per_region)
 
-# Plotting using Matplotlib and Seaborn
+# Plotando usando Matplotlib e Seaborn
 sns.set_context('paper')
 a = sns.relplot(x='Hour', y='NO2', data=air_quality_merge, kind='line', hue='Region')
 a.fig.suptitle('NO2 (µg/m³) per hour in São Paulo')
